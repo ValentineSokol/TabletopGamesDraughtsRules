880d533459549902bc03414a634b175f293d7d5f
ValentineSokol <valentinesokolovskiy@gmail.com>
2022-02-14 07:34:39 +0200

    initial

---
+++ Move/Move.ts
@@ -0,0 +1,77 @@
+import Position from '../Position/Position';
+import Board from '../Board/Board';
+import Piece from '../Pieces/Piece';
+import IMove from '../interfaces/IMove';
+
+class Move implements IMove {
+  protected readonly board: Board;
+
+  public from: Position;
+
+  public to: Position;
+
+  public capturedPiecePosition: Position | null = null;
+
+  public capturedPiece: Piece | null = null;
+
+  constructor(
+    board: Board,
+    from: Position,
+    to: Position,
+    capturedPiecePosition?: Position,
+  ) {
+    this.board = board;
+    this.from = from;
+    this.to = to;
+
+    if (capturedPiecePosition) {
+      this.capturedPiecePosition = capturedPiecePosition;
+    }
+  }
+
+  public execute() : any {
+    let chainCaptures : Move[] = [];
+    const movingPiece = this.board.getPieceAtPosition(this.from);
+    if (!movingPiece) return {};
+
+    this.board.movePiece(this);
+    if (Board.hasPiecePromoted(movingPiece)) this.board.promotePiece(movingPiece);
+
+    if (this.capturedPiecePosition) {
+      const capturedPiece = this.board.getPieceAtPosition(this.capturedPiecePosition);
+      if (capturedPiece) {
+        this.capturedPiece = capturedPiece;
+        this.board.removePiece(capturedPiece);
+        chainCaptures = movingPiece.getAvailableCaptures();
+      }
+    }
+    if (!chainCaptures.length) {
+      this.board.finishMove();
+    }
+
+    return { movedPiece: movingPiece, hasChainCaptures: chainCaptures.length };
+  }
+
+  public revert() {
+    const from = this.to;
+    const to = this.from;
+
+    if (this.capturedPiece) {
+      this.board.placePiece(this.capturedPiece);
+    }
+    this.board.movePiece(new Move(this.board, from, to));
+
+    const movedPiece = this.board.getPieceAtPosition(to);
+    if (movedPiece) {
+      const chainCaptures = movedPiece.getAvailableCaptures();
+      if (!chainCaptures.length) this.board.finishMove();
+    }
+  }
+
+  public toBoardNotation() : string {
+    const delimiter = this.capturedPiecePosition ? 'x' : '-';
+    return `${this.from.toBoardNotation()}${delimiter}${this.to.toBoardNotation()}`;
+  }
+}
+
+export default Move;
---
+++ constants/board.ts
@@ -0,0 +1,8 @@
+import { Diagonal } from '../customTypes';
+
+export const diagonals : Diagonal[] = [
+  { x: -1, y: -1 },
+  { x: 1, y: -1 },
+  { x: -1, y: 1 },
+  { x: 1, y: 1 },
+];
