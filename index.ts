2fb1fc58861e7e9fa8323896415688833468daa5
ValentineSokol <valentinesokolovskiy@gmail.com>
2022-02-14 11:56:55 +0200

    Add missing files

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
 import Board from './Board/Board';
+import PieceType from "./Pieces/Piece";
+import KingType from "./Pieces/King";
+import MoveType from "./Move/Move";
+import ChainMoveType from "./ChainMove/ChainMove";
+import MoveHistoryType from "./MoveHistory/MoveHistory";
+import * as customTypes from './customTypes';
 
 export default Board;
+export type Piece = PieceType;
+export type King = KingType;
+export type Move = MoveType;
+export type ChainMove = ChainMoveType;
+export type MoveHistory = MoveHistoryType;
+export type Diagonal = customTypes.Diagonal;
+export type Side = customTypes.Side;
+export type GameOutcome = customTypes.GameOutcome;
